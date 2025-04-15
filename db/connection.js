53b3fa191087086dce4d568426cc32652a0fa171
Richard Lambert <hello@richardlambert.dev>
2025-04-15 16:14:13 +0100

    :speech_balloon: improve env error output

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,11 @@ setEnvironment();
 const db = new pg.Pool();
 
 if (!process.env.PGDATABASE) {
-  throw new Error('No PGDATABASE configured');
+  console.error(`No PGDATABASE configured
+HINT: Have you setup the required environment variables?
+https://github.com/richardIambert/seed-db/tree/main?tab=readme-ov-file#3-setup-environment-variables
+  `);
+  process.exit(1);
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
