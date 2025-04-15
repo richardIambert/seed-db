f788b9fe0343b5619beac3afe72d97e807b18f3b
Richard Lambert <hello@richardlambert.dev>
2025-04-15 15:36:40 +0100

    :recycle: create table creation utility function

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,6 @@
 import format from 'pg-format';
 import db from '../connection.js';
-import { convertTimestampToDate } from './utils.js';
+import { convertTimestampToDate, createTable } from './utils.js';
 
 const seed = async ({ topicData, userData, articleData, commentData }) => {
   // drop tables
@@ -13,114 +13,100 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
   // topics
   // =======================
 
-  // create table
-  await db.query(`
-    CREATE TABLE topics (
-      slug VARCHAR(255) PRIMARY KEY,
-      description VARCHAR(255) NOT NULL,
-      img_url VARCHAR(1000) NOT NULL
-    );
-  `);
-
-  // reshape data
-  const topicsRows = topicData.map(({ slug, description, img_url }) => [
-    slug,
-    description,
-    img_url,
-  ]);
-
-  // insert data into table
-  await db.query(format(`INSERT INTO topics (slug, description, img_url) VALUES %L`, topicsRows));
+  await createTable(
+    `
+      CREATE TABLE topics (
+        slug VARCHAR(255) PRIMARY KEY,
+        description VARCHAR(255) NOT NULL,
+        img_url VARCHAR(1000) NOT NULL
+      );
+    `,
+    `
+      INSERT INTO topics (slug, description, img_url) 
+      VALUES %L;
+    `,
+    topicData.map(({ slug, description, img_url }) => [slug, description, img_url])
+  );
 
   // =======================
   // users
   // =======================
 
-  // create table
-  await db.query(`
-    CREATE TABLE users (
-      username VARCHAR(255) PRIMARY KEY,
-      name VARCHAR(255) NOT NULL,
-      avatar_url VARCHAR(1000) NOT NULL
-    );
-  `);
-
-  // reshape data
-  const usersRows = userData.map(({ username, name, avatar_url }) => [username, name, avatar_url]);
-
-  // insert data into table
-  await db.query(format(`INSERT INTO users (username, name, avatar_url) VALUES %L`, usersRows));
+  await createTable(
+    `
+      CREATE TABLE users (
+        username VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        avatar_url VARCHAR(1000) NOT NULL
+      );
+    `,
+    `
+      INSERT INTO users (username, name, avatar_url)
+      VALUES %L;
+    `,
+    userData.map(({ username, name, avatar_url }) => [username, name, avatar_url])
+  );
 
   // =======================
   // articles
   // =======================
 
-  // create table
-  await db.query(`
-    CREATE TABLE articles (
-      article_id SERIAL PRIMARY KEY,
-      title VARCHAR(255) NOT NULL,
-      topic VARCHAR(255),
-      FOREIGN KEY (topic) REFERENCES topics(slug),
-      author VARCHAR(255),
-      FOREIGN KEY (author) REFERENCES users(username),
-      body TEXT NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      votes INT DEFAULT 0,
-      article_img_url VARCHAR(1000) NOT NULL
-    );
-  `);
-
-  // reshape data
-  const articlesRows = articleData.map((article) => {
-    const { title, topic, author, body, created_at, votes, article_img_url } =
-      convertTimestampToDate(article);
-    return [title, topic, author, body, created_at, votes, article_img_url];
-  });
-
-  // insert data into table
-  await db.query(
-    format(
-      `INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L`,
-      articlesRows
-    )
+  await createTable(
+    `
+      CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        topic VARCHAR(255),
+        FOREIGN KEY (topic) REFERENCES topics(slug),
+        author VARCHAR(255),
+        FOREIGN KEY (author) REFERENCES users(username),
+        body TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        votes INT DEFAULT 0,
+        article_img_url VARCHAR(1000) NOT NULL
+      );
+    `,
+    `
+      INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url)
+      VALUES %L;
+    `,
+    articleData.map((article) => {
+      const { title, topic, author, body, created_at, votes, article_img_url } =
+        convertTimestampToDate(article);
+      return [title, topic, author, body, created_at, votes, article_img_url];
+    })
   );
 
   // =======================
   // comments
   // =======================
 
-  // create table
-  await db.query(`
-    CREATE TABLE comments (
-      comment_id SERIAL PRIMARY KEY,
-      article_id INT,
-      FOREIGN KEY (article_id) REFERENCES articles(article_id),
-      body TEXT NOT NULL,
-      votes INT DEFAULT 0,
-      author VARCHAR(255),
-      FOREIGN KEY (author) REFERENCES users(username),
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
-
-  // reshape data
-  const commentsRows = await Promise.all(
-    commentData.map(async (comment) => {
-      const { article_title, body, votes, author, created_at } = convertTimestampToDate(comment);
-      const result = await db.query(
-        format(`SELECT article_id FROM articles WHERE title = %L`, article_title)
+  await createTable(
+    `
+      CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY,
+        article_id INT,
+        FOREIGN KEY (article_id) REFERENCES articles(article_id),
+        body TEXT NOT NULL,
+        votes INT DEFAULT 0,
+        author VARCHAR(255),
+        FOREIGN KEY (author) REFERENCES users(username),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
-      const article_id = result.rows[0].article_id;
-      return [article_id, body, votes, author, created_at];
-    })
-  );
-
-  // insert data into table
-  await db.query(
-    format(
-      `INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L`,
-      commentsRows
+    `,
+    `
+      INSERT INTO comments (article_id, body, votes, author, created_at)
+      VALUES %L;
+    `,
+    await Promise.all(
+      commentData.map(async (comment) => {
+        const { article_title, body, votes, author, created_at } = convertTimestampToDate(comment);
+        const result = await db.query(
+          format(`SELECT article_id FROM articles WHERE title = %L`, article_title)
+        );
+        const article_id = result.rows[0].article_id;
+        return [article_id, body, votes, author, created_at];
+      })
     )
   );
 };
--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -1,4 +1,6 @@
 import { config } from 'dotenv';
+import format from 'pg-format';
+import db from '../connection.js';
 
 export const setEnvironment = () => {
   const path = `${process.cwd()}/.env.${process.env.NODE_ENV || 'development'}`;
@@ -8,4 +10,14 @@ export const setEnvironment = () => {
 export const convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   if (!created_at) return { ...otherProperties };
   return { created_at: new Date(created_at), ...otherProperties };
+};
+
+export const createTable = async (createQuery, insertQuery, data) => {
+  try {
+    await db.query(createQuery);
+    await db.query(format(insertQuery, data));
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
